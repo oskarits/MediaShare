ce4954ac5543626e5334b80e9be976e279ce4b0a
ottopoika <36150005+ottopoika@users.noreply.github.com>
2019-04-26 11:45:24 +0300

    Tweaks for login form

--- Global/js/global.js
+++ Global/js/global.js
@@ -1,11 +1,11 @@
 'use strict';
 
 const openNav = () => {
-    document.getElementById("navbar").style.display = 'block';
+    document.getElementById("navbar").style.display = 'inline-block';
     document.getElementById("burger").style.display = 'none';
 };
 
 const closeNav = () => {
     document.getElementById("navbar").style.display = 'none';
-    document.getElementById("burger").style.display = 'block';
+    document.getElementById("burger").style.display = 'inline-block';
 }
