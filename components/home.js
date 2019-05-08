40e73a4fe8f285999faeb9b1691362e110d22d54
Tom Paavolainen <tuomas.paavolainen@metropolia.fi>
2019-05-08 23:09:08 +0300

    foo

--- components/home.js
+++ components/home.js
@@ -1,35 +1,12 @@
-// 'use strict';
-
-// const form = document.querySelector('form');
-// // HTML contains element 'message'. This is used to show the server's response
-// // Select it and save it as a variable/object
-// const message = document.getElementById('message');
-// // make function 'upload' which
-// const image = document.getElementById('image');
-// const upload = (evt) => {
-// // - prevents the form from sending
-//     evt.preventDefault();
-// // - writes 'Upload in progress...' into 'message' element
-//     message.innerHTML = 'Upload in progress...';
-// // - makes FormData -object and adds the file selected byt the user into the object
-//     const fd = new FormData(form);
-// // - send the file to the same url as in task a by using fetch -method
-//     const options = {
-//         method: 'POST',
-//         body: fd,
-//     };
-//     fetch('./image', options)
-//     .then(response => {
-//         return response.json();
-//     }).then(json => {
-//         console.log(json);
-//         message.innerHTML = json.message;
-//         image.src = './uploads/' + json.file.filename;
-//     });
-// // - when file upload is complete, writes server response to 'message' element
-// // function ends
-// };
-
-// // make an event listener which calls upload function when the form is submitted
-
-// form.addEventListener('submit', upload);
+const createArticle = (image, title, texts) => {
+    let text = '';
+    for (let t of texts) {
+      text += `<p>${t}</p>`;
+    }
+  
+    return `<img src="${image}" alt="${title}">
+                  <h3 class="card-title">${title}</h3>
+                  <p>${text}</p>
+                  <p><button>View</button></p>`;
+  };
+  
--- main.js
+++ main.js
@@ -5,8 +5,8 @@ const express = require('express');
 require('dotenv').config();
 const multer = require('multer');
 const upload = multer({ dest: 'components/uploads' });
-const db = require('./components/tools/db');
-const sql = require('./components/tools/sql_tools');
+const db = require('./tools/db');
+const sql = require('./tools/sql_tools');
 const passport = require('passport');
 const session = require('express-session')
 const bParser = require('body-parser');
