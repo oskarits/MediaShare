3a6a919c4f03b0cce510b54ea6a1c3ae2799221f
Tom Paavolainen <tuomas.paavolainen@metropolia.fi>
2019-05-08 23:24:18 +0300

    created js for home feed images

--- components/home.js
+++ components/home.js
@@ -1,12 +1,31 @@
-const createArticle = (image, title, texts) => {
-    let text = '';
-    for (let t of texts) {
-      text += `<p>${t}</p>`;
-    }
-  
-    return `<img src="${image}" alt="${title}">
-                  <h3 class="card-title">${title}</h3>
-                  <p>${text}</p>
-                  <p><button>View</button></p>`;
-  };
-  
+const main = document.getElementsByClassName('image');
+const myRequest = new Request('images.json');
+
+fetch(myRequest)
+.then(function(response) {
+    return response.json();
+}).then((images) => {
+    for (let i = 0; i < images.length; i++) {
+        const img = document.createElement('img');
+        const imgPath = './img/original/' + `${images[i].mediaUrl}`;
+        img.src = imgPath;
+        // img.alt = `${images[i].mediaTitle}`;
+
+        // const caption = document.createElement('figcaption');
+        // caption.innerHTML = `${images[i].mediaTitle}`;
+
+        // const figure = document.createElement('figure');
+        // const title = document.createElement('h3');
+        // title.innerHTML = `${images[i].mediaTitle}`;
+
+        // figure.appendChild(img);
+        // figure.appendChild(caption);
+        // figure.appendChild(title);
+
+        main.appendChild(img);
+    };
+
+
+        // 
+    
+});
