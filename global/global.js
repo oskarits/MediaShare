236fdb11c6f5ca44db6a1b593b52b8d7563fe631
Tom Paavolainen <tuomas.paavolainen@metropolia.fi>
2019-05-14 13:46:11 +0300

    navbar animation

--- global/global.js
+++ global/global.js
@@ -1,18 +1,20 @@
 'use strict';
 
 const openNav = () => {
-    document.getElementById("navbar").style.height = 'auto';
     document.getElementById("navbar").style.display = 'flex';
+    document.getElementById("navbar").style.opacity = '1';
     document.getElementById("burger").style.display = 'none';
     document.getElementById("closeBtn").style.display = 'inline-block';
-    
+    const reverseClass = document.getElementById("navbar");
+
+    reverseClass.classList.add('reverse');
 };
 
 const closeNav = () => {
-    document.getElementById("navbar").style.height = '0px';
     document.getElementById("navbar").style.display = 'none';
     document.getElementById("closeBtn").style.display = 'none';
     document.getElementById("burger").style.display = 'inline-block';
-    
+    document.getElementById("navbar").style.opacity = '0';
 
+    
 }
